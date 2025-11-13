c8d43a377a35b8ff3dd11ca5820bfa2bdbced49f
wayneballard <126793814+wayneballard@users.noreply.github.com>
2025-11-13 18:15:30 +0100

    Update SearchInsertPosition.py

--- SearchInsertPosition.py
+++ SearchInsertPosition.py
@@ -1,28 +1,29 @@
-#O(N) time complexity
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        size = []
+        larger = False
+        length_nums = len(nums)
+        length = None
 
-nums = [1,3,5,6]
-target = 6.5
+        for i in range(length_nums):
+            if nums[i] == target:
+                return i  # Found the target, return its index
+            elif nums[i] > target:
+                size.append(nums[i])
+                length = len(size)
+            elif nums[length_nums - 1] < target:
+                larger = True
 
-size = []
-larger = False
+        if size:
+            target_index_number = length_nums - length
+            return target_index_number
 
-length_nums = len(nums)
-length = None
+        if larger:
+            return length_nums
 
-
-for i in range(length_nums):
-    if nums[i] == target:
-        print(f"The target number is found at index position {i}")
-        break
-    elif nums[i] > target:
-        size.append(nums[i])
-        length = (len(size))
-    elif nums[length_nums-1] < target:
-        larger = True
-
-if size:
-    target_index_number = length_nums - length
-    print(target_index_number)
-
-if larger:
-    print(length_nums)
+        return 0 
